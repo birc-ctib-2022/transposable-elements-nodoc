4e15a2fc300840ed018a9cca3302c15b72f80710
Anton Homilius <antonhomilius@Antons-MacBook-Air.local>
2022-12-07 12:24:35 +0100

    test

--- src/genome.py
+++ src/genome.py
@@ -113,7 +113,7 @@ class ListGenome():
     def disable_te(self, te: int):
         dis_te = self.TE_dict[te]
         for i in range(dis_te[0], dis_te[0]+dis_te[1]):
-            self.genome[i] = 'I'
+            self.genome[i] = 'x'
         self.TE_dict.pop(te)
         
     
