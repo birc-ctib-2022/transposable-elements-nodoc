28d932ee06da7169d146d9e371b5252347a003ec
Anton Homilius <antonhomilius@Antons-MacBook-Air.local>
2022-12-07 13:59:03 +0100

    test

--- src/genome.py
+++ src/genome.py
@@ -104,7 +104,6 @@ class ListGenome():
     def copy_te(self, te: int, offset: int):
         if te in self.TE_dict:
             element = self.TE_dict[te]
-            clone_start = (element[0] + offset) % len(self)
             clone_start = clone_start if clone_start > 0 else clone_start + 1
             clone_length = element[1]
             self.insert_te(clone_start, clone_length)
