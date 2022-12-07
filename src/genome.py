a7f380a84ee6a9e3d5181617b9b1f130b743402a
Anton Homilius <antonhomilius@Antons-MacBook-Air.local>
2022-12-07 12:21:22 +0100

    test

--- src/genome.py
+++ src/genome.py
@@ -2,9 +2,79 @@
 # Libraries:
 
 from collections import defaultdict
+from abc import ABC, abstractmethod
 
 ################################################
 # Functions/Classes:
+
+
+class Genome(ABC):
+    """Representation of a circular genome."""
+
+    def __init__(self, n: int):
+        """Create a genome of size n."""
+        ...  # not implemented yet
+        
+    @abstractmethod
+    def insert_te(self, pos: int, length: int) -> int:
+        """
+        Insert a new transposable element.
+        Insert a new transposable element at position pos and len
+        nucleotide forward.
+        If the TE collides with an existing TE, i.e. genome[pos]
+        already contains TEs, then that TE should be disabled and
+        removed from the set of active TEs.
+        Returns a new ID for the transposable element.
+        """
+        ...  # not implemented yet
+
+    @abstractmethod
+    def copy_te(self, te: int, offset: int) -> int | None:
+        """
+        Copy a transposable element.
+        Copy the transposable element te to an offset from its current
+        location.
+        The offset can be positive or negative; if positive the te is copied
+        upwards and if negative it is copied downwards. If the offset moves
+        the copy left of index 0 or right of the largest index, it should
+        wrap around, since the genome is circular.
+        If te is not active, return None (and do not copy it).
+        """
+        ...  # not implemented yet
+
+    @abstractmethod
+    def disable_te(self, te: int) -> None:
+        """
+        Disable a TE.
+        If te is an active TE, then make it inactive. Inactive
+        TEs are already inactive, so there is no need to do anything
+        for those.
+        """
+        ...  # not implemented yet
+
+    @abstractmethod
+    def active_tes(self) -> list[int]:
+        """Get the active TE IDs."""
+        ...  # not implemented yet
+
+    @abstractmethod
+    def __len__(self) -> int:
+        """Get the current length of the genome."""
+        ...  # not implemented yet
+
+    @abstractmethod
+    def __str__(self) -> str:
+        """
+        Return a string representation of the genome.
+        Create a string that represents the genome. By nature, it will be
+        linear, but imagine that the last character is immidiatetly followed
+        by the first.
+        The genome should start at position 0. Locations with no TE should be
+        represented with the character '-', active TEs with 'A', and disabled
+        TEs with 'x'.
+        """
+        ...  # not implemented yet
+
 
 class ListGenome():
 
