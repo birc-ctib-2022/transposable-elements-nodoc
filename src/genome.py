efc7ddece67b4a81eb2310cdc5e75bf78d064f22
Anton Homilius <antonhomilius@Antons-MacBook-Air.local>
2022-12-07 13:47:35 +0100

    test

--- src/genome.py
+++ src/genome.py
@@ -106,7 +106,7 @@ class ListGenome():
     def copy_te(self, te: int, offset: int):
         if te in self.TE_dict:
             element = self.TE_dict[te]
-            clone_start = abs(element[0] + offset) % len(self)
+            clone_start = (element[0] + offset) % len(self)
             clone_length = element[1]
             self.insert_te(clone_start, clone_length)
             return self.TE_ID
@@ -201,7 +201,7 @@ class LinkedListGenome():
     def copy_te(self, te: int, offset: int):
         if te in self.TE_dict:
             element = self.TE_dict[te]
-            clone_start = abs(element[0] + offset) % len(self)
+            clone_start = (element[0] + offset) % len(self)
             clone_length = element[1]
             self.insert_te(clone_start, clone_length)
             return self.TE_ID
